f5ee4ed555cfbb1e0b9cd58450fddb42d6d8fe25
karpach015 <3veryTh!ng>
2021-04-03 23:22:45 +0300

    TEST test performance commit

--- main.py
+++ main.py
@@ -17,24 +17,9 @@ async def get_new_urls(wait_for):
     prev_urls = set()
     while True:
         await asyncio.sleep(wait_for)
+
         response = requests.get(parse_url)
-        urls = set(Bs(response.content, 'html.parser').select("url loc"))
-
-        if len(urls - prev_urls) > 0 and len(prev_urls) != 0:
-            all_urls = [str(url.text) for url in (urls - prev_urls) if "/coins" in url.text]
-            prev_urls = urls
-
-            print(type(prev_urls[0]))
-            print(type(urls[0]))
-
-            urls_in_package_of_15 = []
-            for i in range(len(all_urls) // 16 + 1):
-                urls_in_package_of_15.append(all_urls[16*i:16*i+15])
-
-            for urls_for_message in urls_in_package_of_15:
-                message = "\n".join(urls_for_message)
-                # print(message)
-                await bot.send_message("-1001319115779", f"Новый ссылки:\n{message}")
+        html = Bs(response.content, 'html.parser')
 
 
 if __name__ == '__main__':
